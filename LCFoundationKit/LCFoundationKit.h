fca6b50a4ea19c162fc591afbdebcc718f19a227
Christoph Lauterbach <christoph@MacBook-Pro.speedport.ip>
2014-08-11 12:16:37 +0200

    Initial Commit

---
+++ LCFoundationKit/LCFoundationKit.h
@@ -0,0 +1,13 @@
+//
+//  LCFoundationKit.h
+//  LCFoundationKit
+//
+//  Created by Christoph Lauterbach on 11.08.14.
+//  Copyright (c) 2014 Christoph Lauterbach. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LCFoundationKit : NSObject
+
+@end
---
+++ LCFoundationKit/LCFoundationKit.m
@@ -0,0 +1,13 @@
+//
+//  LCFoundationKit.m
+//  LCFoundationKit
+//
+//  Created by Christoph Lauterbach on 11.08.14.
+//  Copyright (c) 2014 Christoph Lauterbach. All rights reserved.
+//
+
+#import "LCFoundationKit.h"
+
+@implementation LCFoundationKit
+
+@end
