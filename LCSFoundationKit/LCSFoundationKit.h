68b92da9ca02fc3743a0646cddbe79e9c8041194
Christoph Lauterbach <c.s.lauterbach@googlemail.com>
2014-08-20 10:49:48 +0200

    Fixed target settings

--- LCSFoundationKit/LCSFoundationKit.h
+++ LCSFoundationKit/LCSFoundationKit.h
@@ -2,11 +2,11 @@
 //  LCSFoundationKit.h
 //  LCSFoundationKit
 //
-//  Created by Christoph Lauterbach on 18.08.14.
+//  Created by Christoph Lauterbach on 20.08.14.
 //  Copyright (c) 2014 Christoph Lauterbach. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
 
 //! Project version number for LCSFoundationKit.
 FOUNDATION_EXPORT double LCSFoundationKitVersionNumber;
@@ -14,12 +14,6 @@ FOUNDATION_EXPORT double LCSFoundationKitVersionNumber;
 //! Project version string for LCSFoundationKit.
 FOUNDATION_EXPORT const unsigned char LCSFoundationKitVersionString[];
 
-#import <LCSFoundationKit/NSArray+LCSFoundationKit.h>
-#import <LCSFoundationKit/NSCharacterSet+LCSFoundationKit.h>
-#import <LCSFoundationKit/NSData+LCSFoundationKit.h>
-#import <LCSFoundationKit/NSDictionary+LCSFoundationKit.h>
-#import <LCSFoundationKit/CGGeometry_LCSFoundationKit.h>
-#import <LCSFoundationKit/NSIndexSet+LCSFoundationKit.h>
-#import <LCSFoundationKit/NSRegularExpression+LCSFoundationKit.h>
-#import <LCSFoundationKit/NSString+LCSFoundationKit.h>
-#import <LCSFoundationKit/NSURL+LCSFoundationKit.h>
+// In this header, you should import all the public headers of your framework using statements like #import <LCSFoundationKit/PublicHeader.h>
+
+
